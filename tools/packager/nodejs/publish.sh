d8a3ecb0b84db64503c197a5e86722203c12738c
joaovitubr <joaovmachado10@gmail.com>
2025-12-03 10:54:56 -0300

    refactor(ci): replaced hardcoded preview tag with PACKAGE_TAG variable

--- tools/packager/nodejs/publish.sh
+++ tools/packager/nodejs/publish.sh
@@ -3,9 +3,4 @@
 cd $DIST_DIR/nodejs
 
 npm i
-
-if [ -n "$PREVIEW_VERSION" ]; then
-    npm publish --provenance --access public --tag preview
-else
-    npm publish --provenance --access public --tag latest
-fi
+npm publish --provenance --access public --tag $PACKAGE_TAG
